5300b6505dec96d4d7207f3da9389f0315361775
FlorianLB <flonqueubrochard@clever-age.com>
2014-09-16 11:36:15 +0200

    host the box on dropbox

--- dist/files/Vagrantfile.tpl.php
+++ dist/files/Vagrantfile.tpl.php
@@ -7,9 +7,8 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "virtualbox-debian-wheezy-64"
 
-  # URL auquel télécharger la basebox si non disponible sur la machine hôte
   # Debian Wheezy 7.0 amd64 - Vanilla
-  config.vm.box_url = "http://home.clever-age.net/~jkruppa/debian-wheezy-64.box"
+  config.vm.box_url = "https://dl.dropboxusercontent.com/u/10765492/debian-wheezy-64.box"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
@@ -19,22 +18,20 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
 
-  # Adresse IP de la VM
+  # IP Address of the VM
   config.vm.network "private_network", ip: "<?php echo $general['address_ip']; ?>"
 
-  # Nom d'hôte de la VM (utilisé dans l'inventaire de la conf Ansible ansible/dev)
+  # Hostname of the VM (use in ansible inventory : ansible/dev)
   config.vm.hostname = "<?php echo $general['vm_name']; ?>"
 
-  # Désactivation du partage par défaut
+  # Deactivation of the default share
   config.vm.synced_folder ".", "/vagrant", id: "vagrant-root", disabled: true
 
-  # Partage du dossier courant dans la home de l'utilisateur vagrant
+  # Share of the current folder
   config.vm.synced_folder ".", "/home/vagrant/www/project", type: "nfs"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
-  # Example for VirtualBox:
-  #
   config.vm.provider "virtualbox" do |vb|
      # Don't boot with headless mode
      vb.gui = false
@@ -60,12 +57,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   # PLUGINS
 
-  # Vérification et mise à jour si nécessaire des guest additions Virtualbox
-  # au démarrage de la VM
   config.vbguest.auto_update = true
-
-  # On autorise le plugin vagrant-vbguest à télécharger un ISO des guest additions
-  # sur un serveur distant
   config.vbguest.no_remote = false
 
 end
