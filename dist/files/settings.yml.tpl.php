2ee129772e0c81bc4542977fbdeefd763666f9b4
Jean Yves GASTAUD <jygastaud@gmail.com>
2015-04-20 23:29:45 +0200

    Add default doc_root for drupal and symfony projects.

--- dist/files/settings.yml.tpl.php
+++ dist/files/settings.yml.tpl.php
@@ -3,11 +3,17 @@
 user: "vagrant"
 
 host_name: "<?php echo $general['hostname']; ?>"
-doc_root: "/home/vagrant/www/project<?php echo $general['root_folder']; ?>"
 log_dir: "/home/vagrant/logs"
 framework: <?php if (!empty($framework)) : ?>
  "<?php echo $framework; ?>"
 <?php endif; ?>
+<?php if (!empty($framework) && 'drupal7' === $framework) : ?>
+doc_root: "/home/vagrant/www/project"
+<?php endif; ?>
+<?php if (!empty($framework) && 'symfony2' === $framework) : ?>
+doc_root: "/home/vagrant/www/project/web"
+<?php endif; ?>
+
 
 # Nginx
 nginx_port_dashboard: "1000"
